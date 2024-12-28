1c8c04912b748062a19ae69546d0ee9c85e8fdbc
玖数运算 <149989064+jiushu520@users.noreply.github.com>
2024-12-28 11:33:23 +0800

    Add files via upload

--- accounts.js
+++ accounts.js
@@ -1,152 +1,101 @@
 const accounts = {
+  '玖数': { password: 'jiushu', expiry: '2026-1-31' },
+  '天佑城': { password: 'jiushu', expiry: '2026-1-31' },
+  '京师': { password: 'jiushu', expiry: '2026-1-31' },
+  '外滩': { password: 'jiushu', expiry: '2026-1-31' },
+  '嘉信': { password: 'jiushu', expiry: '2026-1-31' },
 
-  '玖数':'jiushu',
-  '天佑城':'jiushu',
-  '京师':'jiushu',
-  '外滩':'jiushu',
-  '嘉信':'jiushu',
+  '吴钰涵': { password: 'jiushu', expiry: '2025-1-31' },
+  '涂子玥': { password: 'jiushu', expiry: '2025-1-31' },
+  '邓康禾': { password: 'jiushu', expiry: '2025-1-31' },
+  '陈思霖': { password: 'jiushu', expiry: '2025-1-31' },
+  '胡一诺': { password: 'jiushu', expiry: '2025-1-31' },
+  '杨开欣': { password: 'jiushu', expiry: '2025-1-31' },
+  '李函泽': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '胡浩轩': { password: 'jiushu', expiry: '2025-1-31' },
+  '梁佳颖': { password: 'jiushu', expiry: '2025-1-31' },
+  '黄卓雅': { password: 'jiushu', expiry: '2025-1-31' },
+  '王乐思': { password: 'jiushu', expiry: '2025-1-31' },
+  '罗锦昊': { password: 'jiushu', expiry: '2025-1-31' },
+  '胡藩龄': { password: 'jiushu', expiry: '2025-1-31' },
+  '廖彦智': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '丘昭钧': { password: 'jiushu', expiry: '2025-1-31' },
+  '杨耀博': { password: 'jiushu', expiry: '2025-1-31' },
+  '梁芷晞': { password: 'jiushu', expiry: '2025-1-31' },
+  '方子轩': { password: 'jiushu', expiry: '2025-1-31' },
+  '孙成亮': { password: 'jiushu', expiry: '2025-1-31' },
+  '张鸣扬': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '梁芷殷': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '李恩颐': { password: 'jiushu', expiry: '2025-1-31' },
+  '区睿乐': { password: 'jiushu', expiry: '2025-1-31' },
+  '詹立仑': { password: 'jiushu', expiry: '2025-1-31' },
+  '冯宇晴': { password: 'jiushu', expiry: '2025-1-31' },
+  '唐清芷': { password: 'jiushu', expiry: '2025-1-31' },
+  '王昀靖': { password: 'jiushu', expiry: '2025-1-31' },
+  '郭承明': { password: 'jiushu', expiry: '2025-1-31' },
+  '温丹诚': { password: 'jiushu', expiry: '2025-1-31' },
+  '欧阳可欣': { password: 'jiushu', expiry: '2025-1-31' },
+  '王瑞彬': { password: 'jiushu', expiry: '2025-1-31' },
+  '李晨希': { password: 'jiushu', expiry: '2025-1-31' },
+  '古一铭': { password: 'jiushu', expiry: '2025-1-31' },
+  '卢嘉阳': { password: 'jiushu', expiry: '2025-1-31' },
+  '陈子诚': { password: 'jiushu', expiry: '2025-1-31' },
+  '夏吉喆': { password: 'jiushu', expiry: '2025-1-31' },
+  '黄荞茵': { password: 'jiushu', expiry: '2025-1-31' },
+  '王俊熙': { password: 'jiushu', expiry: '2025-1-31' },
+  '龙沐洋': { password: 'jiushu', expiry: '2025-1-31' },
+  '马睿柏': { password: 'jiushu', expiry: '2025-1-31' },
+  '王胡睿': { password: 'jiushu', expiry: '2025-1-31' },
+  '李宸睿': { password: 'jiushu', expiry: '2025-1-31' },
+  '叶子轩': { password: 'jiushu', expiry: '2025-1-31' },
 
-  
-'吴钰涵':'jiushu',
-'涂子玥':'jiushu',
-'邓康禾':'jiushu',
-'陈思霖':'jiushu',
-'胡一诺':'jiushu',
-'杨开欣':'jiushu',
-'李函泽':'jiushu',
-'丁士钊':'jiushu',
+  '刘后俊': { password: 'jiushu', expiry: '2025-1-31' },
+  '林鸿博': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '喻孜涵': { password: 'jiushu', expiry: '2025-1-31' },
+  '曹焕彤': { password: 'jiushu', expiry: '2025-1-31' },
+  '冯梓皓': { password: 'jiushu', expiry: '2025-1-31' },
+  '何星亿': { password: 'jiushu', expiry: '2025-1-31' },
+  '刘彦涵': { password: 'jiushu', expiry: '2025-1-31' },
+  '熊威': { password: 'jiushu', expiry: '2025-1-31' },
+  '邬雨轩': { password: 'jiushu', expiry: '2025-1-31' },
 
-  
+  '林逸然': { password: 'jiushu', expiry: '2025-1-31' },
+  '吴忻宸': { password: 'jiushu', expiry: '2025-1-31' },
+  '郭景祺': { password: 'jiushu', expiry: '2025-1-31' },
+  '何星亿': { password: 'jiushu', expiry: '2025-1-31' },
+  '林采盈': { password: 'jiushu', expiry: '2025-1-31' },
+  '邓致': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '李岱轩': { password: 'jiushu', expiry: '2025-1-31' },
+  '俞新旸': { password: 'jiushu', expiry: '2025-1-31' },
 
-'邓语乔':'jiushu',
-'解淇茗':'jiushu',
-'胡浩轩':'jiushu',
-'梁佳颖':'jiushu',
-'黄卓雅':'jiushu',
-'王乐思':'jiushu',
-'罗锦昊':'jiushu',
-'胡藩龄':'jiushu',
-'廖彦智':'jiushu',
+  '郭家侨': { password: 'jiushu', expiry: '2025-1-31' },
+  '郭笑言': { password: 'jiushu', expiry: '2025-1-31' },
+  '邓康婧': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '窦晨曦': { password: 'jiushu', expiry: '2025-1-31' },
+  '窦晨航': { password: 'jiushu', expiry: '2025-1-31' },
+  '郭竣轩': { password: 'jiushu', expiry: '2025-1-31' },
+  '包官悦': { password: 'jiushu', expiry: '2025-1-31' },
+  '廖隽仪': { password: 'jiushu', expiry: '2025-1-31' },
 
-'丘昭钧':'jiushu',
-'杨耀博':'jiushu',
-'梁芷晞':'jiushu',
-'方子轩':'jiushu',
-'孙成亮':'jiushu',
-'张鸣扬':'jiushu',
+  '罗铭曦': { password: 'jiushu', expiry: '2025-1-31' },
+  '陈一鸣': { password: 'jiushu', expiry: '2025-1-31' },
+  '郭佳逸': { password: 'jiushu', expiry: '2025-1-31' },
+  '邓庆凯': { password: 'jiushu', expiry: '2025-1-31' },
+  '何凯晴': { password: 'jiushu', expiry: '2025-1-31' },
+  '胡妍雅': { password: 'jiushu', expiry: '2025-1-31' },
 
+  '李祥宇': { password: 'jiushu', expiry: '2025-1-31' },
+  '黎晓乐': { password: 'jiushu', expiry: '2025-1-31' },
+  '刘湘宇': { password: 'jiushu', expiry: '2025-1-31' },
+  '杨钰峰': { password: 'jiushu', expiry: '2025-1-31' },
+  '欧阳家俊': { password: 'jiushu', expiry: '2025-1-31' },
 
-'梁芷殷':'jiushu',
-
-
-
-
-
-'李恩颐':'jiushu',
-'区睿乐':'jiushu',
-'詹立仑':'jiushu',
-'冯宇晴':'jiushu',
-'唐清芷':'jiushu',
-'王昀靖':'jiushu',
-'郭承明':'jiushu',
-'温丹诚':'jiushu',
-'欧阳可欣':'jiushu',
-'王瑞彬':'jiushu',
-'李晨希':'jiushu',
-'古一铭':'jiushu',
-'卢嘉阳':'jiushu',
-'陈子诚':'jiushu',
-'李琳':'jiushu',
-'黄荞茵':'jiushu',
-'王俊熙':'jiushu',
-'龙沐洋':'jiushu',
-'马睿柏':'jiushu',
-'王胡睿':'jiushu',
-'李宸睿':'jiushu',
-'陈家成':'jiushu',
-
-  
-
-
-  
-
-'刘后俊':'jiushu',
-'林鸿博':'jiushu',
-
-  
-
-
-'喻孜涵':'jiushu',
-'曹焕彤':'jiushu',
-'冯梓皓':'jiushu',
-'何星亿':'jiushu',
-'刘彦涵':'jiushu',
-'熊威':'jiushu',
-'刘君奕':'jiushu',
-
-
-'林逸然':'jiushu',
-'吴忻宸':'jiushu',
-'郭景祺':'jiushu',
-'何星亿':'jiushu',
-'林采盈':'jiushu',
-'邓致':'jiushu',
-
-
-'李岱轩':'jiushu',
-'俞新旸':'jiushu',
-'陈家成':'jiushu',
-
-'郭家侨':'jiushu',
-'郭笑言':'jiushu',
-'杨家朗':'jiushu',
-
-
-
-
-'窦晨曦':'jiushu',
-'窦晨航':'jiushu',
-'郭竣轩':'jiushu',
-'包官悦':'jiushu',
-'廖隽仪':'jiushu',
-
-
-
-
-
-
-'罗铭曦':'jiushu',
-'陈一鸣':'jiushu',
-'郭佳逸':'jiushu',
-'邓庆凯':'jiushu',
-'何凯晴':'jiushu',
-'胡妍雅':'jiushu',
-
-
-'邓康柠':'jiushu',
-'李祥宇':'jiushu',
-'黎晓乐':'jiushu',
-'刘湘宇':'jiushu',
-'杨钰峰':'jiushu',
-'欧阳家俊':'jiushu',
-
-'陈伟滔':'jiushu',
-  
-
-
-
-  
-
-
-
-  
-
-
+  '陈伟滔': { password: 'jiushu', expiry: '2025-1-31' }
 };
