9d2e8d25f3e575b6b1d739528170d141d967decf
玖数运算 <149989064+jiushu520@users.noreply.github.com>
2024-12-29 17:16:35 +0800

    优化chtat

---
+++ chat/proverbs.js
@@ -0,0 +1,22 @@
+// proverbs.js
+
+const mathProverbs = 
+[
+    "a^3 + b^3 = (a+b)(a^2-ab+b^2)",
+    "(a+bi)(c+di)=(ac-bd)+(ad+bc)i",
+   "\\sum_{n=1}^{n}a= \\frac{n(n+1)}{2}",
+    "\\frac{A}{\\sqrt{A^2+B^2}}x+\\frac{B}{\\sqrt{A^2+B^2}}y=\\frac{Ax_0+By_0}{\\sqrt{A^2+B^2}}",
+
+
+     "\\textcolor{red}{(a^2+b^2)}\\textcolor{blue}{(m^2+n^2)}=(\\textcolor{red}{a}\\textcolor{blue}{m}+\\textcolor{red}{b}\\textcolor{blue}{n})^2+(\\textcolor{red}{a}\\textcolor{blue}{n}-\\textcolor{red}{b}\\textcolor{blue}{m})^2\\\\=(\\textcolor{red}{a}\\textcolor{blue}{n}+\\textcolor{red}{b}\\textcolor{blue}{m})^2+(\\textcolor{red}{a}\\textcolor{blue}{m}-\\textcolor{red}{b}\\textcolor{blue}{n})^2",
+
+    "S▱=\\left|\\begin{matrix}x_1&y_1\\\\x_2&y_2\\end{matrix}\\right|=x_1y_2-x_2y_1",
+     "tan\\alpha=\\frac{\\vec a \\otimes\\vec b}{\\vec a\\cdot \\vec b}",
+     "\\begin{bmatrix}\\textcolor{red}{\\frac{A}{\\sqrt{A^2+B^2}}} & \\textcolor{blue}{\\frac{B}{\\sqrt{A^2+B^2}}}\\\\\\textcolor{blue}{-\\frac{B}{\\sqrt{A^2+B^2}}} & \\textcolor{red}{\\frac{A}{\\sqrt{A^2+B^2}}}\\end{bmatrix}\\begin{bmatrix}x_0\\\\y_0\\end{bmatrix}=\\begin{bmatrix}x \\\\y\\end{bmatrix}",
+
+"\\begin{bmatrix}\\textcolor{red}{cos\\alpha} & \\textcolor{blue}{-sin\\alpha}  \\\\\\textcolor{blue}{sin\\alpha} & \\textcolor{red}{cos\\alpha}\\end{bmatrix}\\begin{bmatrix}x  \\\\y\\end{bmatrix}=\\begin{bmatrix}x_0\\\\y_0 \\end{bmatrix}",
+
+
+
+
+  　　　　　　 ];
